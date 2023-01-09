c84c42a5deeac725e41a9cc6dd8df568db0aeaf3
Tanmay Vaij <tanmayvaij22@gmail.com>
2023-01-09 18:00:18 +0530

    added fonts and adjusted page

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,10 +1,23 @@
 export default function LandingPage() {
     return (
-        <div id="landing_page" className="flex items-center justify-center min-h-screen flex-col px-8">
-            <h1 className="text-5xl font-bold">BooksTown</h1>
-            <p className="my-2 text-lg text-center">Best place for second hand books.</p>
-            <img className="w-[30rem] my-10" src="landing-img.png" alt="" />
-            <button className="bg-red-500 hover:bg-red-600 text-white rounded-xl px-6 py-3">Get Started</button>
+        <div id="landing_page" className="flex items-center justify-center min-h-screen px-8 flex-wrap">
+
+            <div className="m-5 space-y-5">
+                <span></span>
+                <h1 id="title" className="text-5xl font-bold">BooksTown</h1>
+                <p className="my-2 text-lg text-center">Best place for buying and selling second hand books.</p>
+                <div>
+                    <button className="bg-orange-500 hover:bg-orange-600 text-white rounded-xl px-6 py-3">    
+                        Start Buying
+                    </button>
+                    <button className="bg-red-500 ml-5 hover:bg-red-600 text-white rounded-xl px-6 py-3">    
+                        Start Selling
+                    </button>
+                </div>
+            </div>
+
+            <img className="w-[30rem] my-10 m-5" src="landing-img.png" alt="" />
+
         </div>
     )
 }
